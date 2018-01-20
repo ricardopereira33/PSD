143c4b9c1c09ea889285bf4552930c6a9377d1f8
Ricardo Pereira <ricardo.agp.914@gmail.com>
2018-01-20 23:07:41 +0000

    make front-end server more beautiful

--- TP/front-end/server.erl
+++ TP/front-end/server.erl
@@ -1,37 +1,34 @@
 -module(server).
--export([server/1]).
+-export([server/2]).
 
 %=================
 % Server Configs : initialize erlzmq context, and bind a socket for accept connections.
-server(Port) ->
+server(Port, Broker) ->
     LoginManager = spawn(fun()-> loginManager(#{"ri" => {"33", false}}) end),
     process_flag(trap_exit, true),
     {ok, LSock} = gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}]),
-    acceptor(LSock, LoginManager).
+    acceptor(LSock, LoginManager, Broker).
 
 %===========
 % process runing for accpet new connects
-acceptor(LSock, LoginManager) ->
+acceptor(LSock, LoginManager, Broker) ->
     {ok, Sock} = gen_tcp:accept(LSock),
-    spawn(fun() -> acceptor(LSock, LoginManager) end),
-    waitLogin(LoginManager, Sock).
+    spawn(fun() -> acceptor(LSock, LoginManager, Broker) end),
+    waitLogin(LoginManager, Sock, Broker).
 
 
 %================================
 % Receive all message and process them. The message are decoded/encoded by ProcolBuffers.
-waitLogin(LoginManager, Sock) ->
+waitLogin(LoginManager, Sock, Broker) ->
     case gen_tcp:recv(Sock,0) of
         {ok, Data} ->
-            io:format("Data: ~p\n",[Data]),
             Map = protos:decode_msg(Data,'MsgCS'),
             case maps:find(type, Map) of
                 {ok,"1"} ->
-                    io:format("Type1\n"),
                     Bin = protos:encode_msg(#{type=>"2", repL=>#{valid => true, msg => "User&Pass"}}, 'MsgCS'),
                     gen_tcp:send(Sock, Bin),
-                    waitLogin(LoginManager, Sock);
+                    waitLogin(LoginManager, Sock, Broker);
                 {ok,"2"} ->
-                    io:format("Type2\n"),
                     {ok, MapUser} = maps:find(info, Map),
                     {ok, User} = maps:find(user, MapUser),
                     {ok, Pass} = maps:find(pass, MapUser),
@@ -42,16 +39,16 @@ waitLogin(LoginManager, Sock) ->
                             gen_tcp:send(Sock, Bin),
                             {ok, Ctx} = erlzmq:context(),
                             {ok, ZmqSock} = erlzmq:socket(Ctx, [pub, {active, false}]),
-                            ok = erlzmq:connect(ZmqSock, "tcp://localhost:3333"),
+                            ok = erlzmq:connect(ZmqSock, "tcp://localhost:"++integer_to_list(Broker)),
                             user(Sock, User, LoginManager, ZmqSock);
                         {_, invalid} ->
                             Bin = protos:encode_msg(#{type=>"2", repL=>#{valid => false, msg => "Invalid"}}, 'MsgCS'),
                             gen_tcp:send(Sock, Bin),
-                            waitLogin(LoginManager, Sock)
+                            waitLogin(LoginManager, Sock, Broker)
                     end;
                 _ ->
-                    io:format("Pack invalid\n"),
-                    waitLogin(LoginManager, Sock)
+                    io:format("Invalid\n"),
+                    waitLogin(LoginManager, Sock, Broker)
             end;
         {error, closed} ->
             io:format("User closed\n")
@@ -65,7 +62,7 @@ loginManager(M) ->
             case maps:find(U, M) of
                 {ok,{P, false}} ->
                     From ! {self(), logged},
-                    io:format("account ~p logged in~n", [U]),
+                    io:format("account ~p logged in\n", [U]),
                     loginManager(maps:update(U,{P,true}, M));
                 _ -> 
                     From ! {self(), invalid},
@@ -73,14 +70,14 @@ loginManager(M) ->
             end;
         {logout, U, From} -> 
             From ! {self(), ok},
-            io:format("account ~p logged out~n", [U]),
+            io:format("account ~p logged out\n", [U]),
             {P,_} = maps:get(U, M),
             loginManager(maps:update(U, {P,false}, M))
     end.
 
 
 %===========
-% User 
+% User : process send orders to Exchange
 user(Sock, User, LoginManager, ZmqSock) ->
     case gen_tcp:recv(Sock, 0) of 
         {ok, Data} ->
@@ -106,6 +103,5 @@ user(Sock, User, LoginManager, ZmqSock) ->
             end;
         {error, closed} ->
             LoginManager ! {logout, User, self()},
-            io:format("User closed")
-
+            io:format("User closed\n")
     end.
