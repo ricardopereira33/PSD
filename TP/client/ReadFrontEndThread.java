8666929b18dd0674dd8cc59831cfef14b4513d06
Dinis <dinispeixoto96@gmail.com>
2018-01-23 12:22:30 +0000

    added client read-frontend thread

---
+++ TP/client/ReadFrontEndThread.java
@@ -0,0 +1,53 @@
+package client;
+
+import client.Protos.MsgCS;
+import client.Protos.Request_Login;
+import client.Protos.Client;
+import client.Protos.Reply_Login;
+import client.Protos.OrderRequest;
+import client.Protos.OrderReply;
+
+import java.io.*;
+import java.net.*;
+
+public class ReadFrontEndThread extends Thread{
+	
+	private Socket frontend;
+	private InputStream inputStream;
+
+	public ReadFrontEndThread(Socket frontend){
+		this.frontend = frontend;
+	}
+
+	public void run(){
+		try{
+			this.inputStream = frontend.getInputStream();
+			while(true){			
+				byte[] b = readMsg();
+				MsgCS msg = MsgCS.parseFrom(b);
+				OrderReply reply = msg.getOrderReply();
+				String notification = reply.getNotification();
+				System.out.println("\n" + notification);
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public byte[] readMsg(){
+        byte[] res = null;
+        try{
+            byte[] b = new byte[1024];
+            int n = inputStream.read(b);
+            
+            res = new byte[n];
+            for(int i = 0; i < n; i++){
+                res[i] = b[i];
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return res;
+    }
+}
